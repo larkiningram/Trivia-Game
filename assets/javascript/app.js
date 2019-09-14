860ef531afd2c584c1fc829d1d64a73690adb343
Larkin <larkiningram@gmail.com>
2019-09-13 19:30:39 -0500

    added submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,65 +12,46 @@ start();
 
 const questions = [
     {
-        question: "Which does not belong?",
-        answers: ["dog", "cat", "parrot", "wolverine"],
-        solution: 3
-    },
-    {
-        question: "Which does not belong?",
-        answers: ["1", "2", "t", "6"],
+        question: "What is Harry Potter's owl's name?",
+        answers: ["Owl", "Crookshanks", "Hedwig", "Pigwidgeon"],
         solution: 2
     },
     {
-        question: "Which does not belong?",
-        answers: ["orange", "bird", "yellow", "red"],
-        solution: 1
-    },
-    {
-        question: "Which does not belong?",
-        answers: ["baker", "cake", "bread", "pie"],
+        question: "Where does Harry get his first wand?",
+        answers: ["Ollivander's", "Flourish and Blotts", "Borgin and Burkes", "Hogwarts"],
         solution: 0
     },
     {
-        question: "Which does not belong?",
-        answers: ["dog", "cat", "parrot", "wolverine"],
+        question: "What position did Hermione play on the Griffindor Quiddich Team",
+        answers: ["keeper", "seeker", "chaser", "none stupid"],
         solution: 3
     },
     {
-        question: "Which does not belong?",
-        answers: ["dog", "cat", "parrot", "wolverine"],
-        solution: 3
-    },
-    {
-        question: "Which does not belong?",
-        answers: ["1", "2", "t", "6"],
+        question: "Which house did Ernie Macmillan belong to?",
+        answers: ["Griffindor", "Slytherin", "Hufflepuff", "Ravenclaw"],
         solution: 2
     },
     {
-        question: "Which does not belong?",
-        answers: ["orange", "bird", "yellow", "red"],
+        question: "Which is not a Deathly Hallow",
+        answers: ["Elder Wand", "Cloak of Invisibility", "Deluminator", "Resurrection Stone"],
+        solution: 2
+    },
+    {
+        question: "How old was Nicholas Flamel when the Socerer's Stone was destroyed?",
+        answers: ["294", "763", "655", "17"],
+        solution: 2
+    },
+    {
+        question: "Which row in the Hall of Prophecies does Harry find his prophecy?",
+        answers: ["96", "97", "196", "197"],
         solution: 1
-    },
-    {
-        question: "Which does not belong?",
-        answers: ["baker", "cake", "bread", "pie"],
-        solution: 0
-    },
-    {
-        question: "Which does not belong?",
-        answers: ["dog", "cat", "parrot", "wolverine"],
-        solution: 3
     }
 ];
 
 //// determine score
 var correctAnswers = [];
-var userAnswers = [];
+var userAnswers = Array(questions.length);
 var score = 0;
-
-for (var i = 0; i < questions.length; i++) {
-    userAnswers[i] = null;
-};
 
 function answerKey() {
     for (i in questions) {
@@ -88,19 +69,18 @@ function userKey() {
         currentQuestion = (parseInt(i) + parseInt(1))
         userIndex = ($("input[name=" + currentQuestion + "]:checked").val());
         userAnswers[i] =(currentQuestion + ": " + questions[i].answers[userIndex]);
-        console.log(userAnswers);
     }
 
-    // console.log(userAnswers);
     return userAnswers;
 };
 
 function userScore() {
     for (i in userAnswers) {
+        currentQuestion = (parseInt(i) + parseInt(1))
         if (userAnswers[i] === correctAnswers[i]) {
             score++;
         }
-        else if (userAnswers[i] === null) {
+        else if (userAnswers[i] === (currentQuestion + ": undefined")) {
             userAnswers[i] = "You forgot this one!";
         }
     }
@@ -112,18 +92,28 @@ function results() {
     answerKey();
     userKey();
     userScore();
+    $("#sub").hide();
     var num = parseInt(questions.length);
-
+    if (score === num) {
+        $(".countdown").html("Congrats! You got a perfect score!");
+    }
+    else if ((score / num) > 0.75) {
+        $(".countdown").html("You did alright");
+    }
+    else if ((score / num) > 0.5) {
+        $(".countdown").html("I guess you passed");
+    }
+    else {
+        $(".countdown").html("ur a failure");
+    }
     $(".questions").html("<h2>Your score: " + score + "/" + num + "</h2> <br></br>");
-    $(".correct").html("Correct Answers <br></br>");
-    $(".user").html("User Answers <br></br>");
+    $(".correct").html("<strong>Correct Answers </strong> <br></br>");
+    $(".user").html("<Strong> User Answers</strong> <br></br>");
     for (i in correctAnswers) {
         $(".user").append(userAnswers[i] + "<br></br>");
         $(".correct").append(correctAnswers[i] + "<br></br>");
 
-    }
-
-
+    };
 };
 
 //// displaying questions and answers
@@ -143,18 +133,22 @@ function tableRow() {
                 $(".answers").append("<br> <br>");
             }
         };
-
-        // console.log(currentQuestion);
-        // console.log(currentAnswers);
+        console.log(currentQuestion);
+        console.log(currentAnswers);
     };
-};
 
-// tableRow();
+    $(".sub").html('<button id="sub" class="btn btn-lg btn-warning"> Submit </button>');
+
+    $("#sub").on("click", function() {
+        stop()
+        results();
+    });
+};
 
 
 /// timing functions 
 
-var time = 10;
+var time = 30;
 var inter;
 
 
